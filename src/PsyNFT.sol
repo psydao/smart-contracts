5058e739be1781143cc96906780cc5a74dc31b9e
Gareth Larkan <gareth@cis.za.net>
2024-03-07 11:05:24 +0200

    Added tests for minting

--- src/PsyNFT.sol
+++ src/PsyNFT.sol
@@ -2,11 +2,47 @@
 pragma solidity 0.8.20;
 
 import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
+import "forge-std/console.sol";
 
-contract PsyNFT is ERC721 {
+contract PsyNFT is ERC721, Ownable2Step {
 
-    constructor() ERC721("PsyNFT", "PSY") {
-        
+    uint256 public tokenId;
+    uint256 public secondLastFibonacci;
+    uint256 public previousFibonacci;
+
+    bool public initialMintCalled;
+
+    constructor() ERC721("PsyNFT", "PSY") Ownable(msg.sender) {
+
+    }
+
+    function initialMint() external onlyOwner {
+        require(!initialMintCalled, "Initial mint completed");
+        uint256 localTokenId = 0;
+
+        for(localTokenId; localTokenId < 5; localTokenId++) {
+            _mint(address(this), localTokenId);
+        }
+
+        tokenId = localTokenId;
+        secondLastFibonacci = 1;
+        previousFibonacci = 2;
+        initialMintCalled = true;
     }
     
+    function mintBatchInFibonacci() external onlyOwner {
+        uint256 batchAmount = secondLastFibonacci + previousFibonacci;
+        uint256 localTokenId = tokenId;
+
+        for(uint256 x; x < batchAmount; x++) {
+            _mint(address(this), localTokenId);
+            localTokenId++;
+        }
+
+        tokenId = localTokenId;
+
+        secondLastFibonacci = previousFibonacci;
+        previousFibonacci = batchAmount;
+    }
 }
--- test/PsyNFT.t.sol
+++ test/PsyNFT.t.sol
@@ -2,8 +2,72 @@
 pragma solidity 0.8.20;
 
 import "forge-std/Test.sol";
+import "../src/PsyNFT.sol";
 
 
 contract PsyNFTTest is Test {
+
+    error OwnableUnauthorizedAccount(address account);
+
+    PsyNFT public psyNFT;
+
+    address owner = vm.addr(1);
+    address alice = vm.addr(2);
+    address bob = vm.addr(3);
+
+    function setUp() public {
+        vm.startPrank(owner);
+        psyNFT = new PsyNFT();
+        vm.stopPrank();
+    }
+
+    function test_MintFailsWhenNonOwner() public {
+        vm.startPrank(alice);
+        vm.expectRevert(abi.encodeWithSelector(OwnableUnauthorizedAccount.selector, address(alice)));
+        psyNFT.initialMint();
+
+        vm.expectRevert(abi.encodeWithSelector(OwnableUnauthorizedAccount.selector, address(alice)));
+        psyNFT.mintBatchInFibonacci();
+
+        vm.stopPrank();
+    }
+
+    function test_InitialMintFailsIfAlreadyCalled() public {
+        vm.startPrank(owner);
+
+        psyNFT.initialMint();
+
+        vm.expectRevert("Initial mint completed");
+        psyNFT.initialMint();
+    }
+
+    function test_PsyNFTCanMintCorrectly() public {
+        assertEq(psyNFT.secondLastFibonacci(), 0);
+        assertEq(psyNFT.previousFibonacci(), 0);
+        assertEq(psyNFT.tokenId(), 0);
+        assertEq(psyNFT.balanceOf(address(psyNFT)), 0);
+
+        vm.startPrank(owner);
+
+        psyNFT.initialMint();
+
+        assertEq(psyNFT.secondLastFibonacci(), 1);
+        assertEq(psyNFT.previousFibonacci(), 2);
+        assertEq(psyNFT.tokenId(), 5);
+        assertEq(psyNFT.balanceOf(address(psyNFT)), 5);
+
+        psyNFT.mintBatchInFibonacci();
+        psyNFT.mintBatchInFibonacci();
+        psyNFT.mintBatchInFibonacci();
+        psyNFT.mintBatchInFibonacci();
+        psyNFT.mintBatchInFibonacci();
+
+        assertEq(psyNFT.secondLastFibonacci(), 13);
+        assertEq(psyNFT.previousFibonacci(), 21);
+        assertEq(psyNFT.tokenId(), 55);
+        assertEq(psyNFT.balanceOf(address(psyNFT)), 55);
+
+        vm.stopPrank();
+    }
    
 }
