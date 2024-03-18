34b19a796240a2af4c1d5874db8827ec9dfd57ee
Gareth Larkan <gareth@cis.za.net>
2024-03-18 14:12:26 +0200

    Reordered function s

--- src/Core.sol
+++ src/Core.sol
@@ -41,19 +41,6 @@ contract Core is Ownable2Step {
         _transfer(_tokenIds, _user);
     }
 
-    function _transfer(uint256[] memory _tokenIds, address _user) internal {
-        psyNFT.transferNFTs(_tokenIds, _user);
-    }
-    
-    function rageQuit(uint256 _tokenId) external {
-        require(rageQuitAllowed, "Treasury: Rage Quit Disabled");
-        treasury.exit(_tokenId, msg.sender);
-    }
-
-    function kick(uint256 _tokenId, address _user) external onlyOwner {
-        treasury.exit(_tokenId, _user);
-    }
-
     function enableRageQuit() external onlyOwner {
         require(!rageQuitAllowed, "Treasury: Rage Quit Already Enabled");
         rageQuitAllowed = true;
@@ -63,4 +50,20 @@ contract Core is Ownable2Step {
         require(rageQuitAllowed, "Treasury: Rage Quit Already Disabled");
         rageQuitAllowed = false;
     }
+
+    function rageQuit(uint256 _tokenId) external {
+        require(rageQuitAllowed, "Treasury: Rage Quit Disabled");
+        treasury.exit(_tokenId, msg.sender);
+    }
+
+    function kick(uint256 _tokenId, address _user) external onlyOwner {
+        treasury.exit(_tokenId, _user);
+    }
+
+    function _transfer(uint256[] memory _tokenIds, address _user) internal {
+        psyNFT.transferNFTs(_tokenIds, _user);
+    }
+    
+    
+
 }
